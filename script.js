a2a2b32f90c83c9da176e22f02310831ea99eb76
Alex Greene <alex@Alexs-MacBook-Pro.local>
2016-05-06 23:41:03 -0700

    drawer progress

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var choice_A = document.getElementById('choice_A_text');
 var choice_B = document.getElementById('choice_B_text');
 var choice_C = document.getElementById('choice_C_text');
 
+function hideDrawer() {
+	drawer.style.display = "none";
+}
 
 function init() {
-
+	//setTimeout(hideDrawer, 2000)
 }
+
+init();
+
